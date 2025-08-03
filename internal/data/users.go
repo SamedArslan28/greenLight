24828f468f55e693abf63ec373e0547d8b3a9177
Abdulsamed Arslan <samedarslan0723@gmail.com>
2025-08-03 18:42:47 +0300

    fix: UserModel Insert function fix

--- internal/data/users.go
+++ internal/data/users.go
@@ -14,9 +14,9 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
-	Password  password  `json:"password"`
+	Password  password  `json:"-"`
 	Activated bool      `json:"activated"`
-	Version   int       `json:"version"`
+	Version   int       `json:"-"`
 }
 
 type password struct {
@@ -82,12 +82,12 @@ func (m UserModel) Insert(user *User) error {
 	query := `
 INSERT INTO users (name, email, password_hash, activated)
 VALUES ($1, $2, $3, $4)
-RETURNING id, created_at, name, email, activated, version;
+RETURNING id, created_at, version;
 `
 	args := []interface{}{user.Name, user.Email, user.Password.hash, user.Activated}
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
-	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.ID, &user.CreatedAt, &user.Name, &user.Email, &user.Activated)
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.ID, &user.CreatedAt, &user.Version)
 	if err != nil {
 		switch {
 		case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
