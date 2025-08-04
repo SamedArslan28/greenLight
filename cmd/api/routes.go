6d0ee3bd6e86d10351050162b195e8dfb5da21a2
Abdulsamed Arslan <samedarslan0723@gmail.com>
2025-08-04 18:43:59 +0300

    feature: add permission to metrics

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -23,6 +23,6 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
 
-	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
+	router.Handler(http.MethodGet, "/debug/vars", app.requirePermission("metrics:view", expvar.Handler().ServeHTTP))
 	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
 }
